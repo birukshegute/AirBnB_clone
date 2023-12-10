b4cda012b9686f7527a278c28713a3ee2a80325e
birukshegute <birukshegute@gmail.com>
2023-12-10 04:47:00 -0800

    engine

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3#
+#!/u1sr/bin/python3
 """Defines the FileStorage class."""
 import json
 from models.base_model import BaseModel
@@ -23,9 +23,28 @@ Class FileStorage:
     
     def all(self):
         """ returns the dictionary __objects"""
+        return FileStorage.__objects
+
     def new(self, obj):
         """ sets in __objects the obj with key <obj class name>.id"""
+        ocname = obj.__class__.__name__
+        FileStorage.__objects["{}.{}".format(ocname, obj.id)] = obj
+
     def save(self):
         """ serializes __objects to the JSON file (path: __file_path)"""
+        odict = FileStorage.__objects
+        objdict = {obj: odict[obj].to_dict() for obj in odict.keys()}
+        with open(FileStorage.__file_path, "w") as f:
+            json.dump(objdict, f)
+
     def reload(self):
         """deserializes the JSON file to __objects"""
+        try:
+            with open(FileStorage.__file_path) as f:
+                objdict = json.load(f)
+                for o in objdict.values():
+                    cls_name = o["__class__"]
+                    del o["__class__"]
+                    self.new(eval(cls_name)(**o))
+        except FileNotFoundError:
+            return
