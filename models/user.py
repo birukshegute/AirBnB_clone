903384b5fc8f8cbff0ce66a5cf15b72ee6b997dc
birukshegute <birukshegute@gmail.com>
2023-12-10 11:19:53 -0800

    user attemptm one

---
+++ models/user.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""A class User that inherits from BaseModel."""
+import BaseModel from base_model
+
+Class User(Basemodel)
+"""A class to register Users
+
+    Attributes:
+    email: string - empty string
+    password: string - empty string
+    first_name: string - empty string
+    last_name: string - empty string"""
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
